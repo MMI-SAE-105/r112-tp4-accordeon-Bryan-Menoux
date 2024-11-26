cd5ff1b650ecb296bcc4ae60b4d3fd9c8f6116eb
Bryan-Menoux <bryan.menoux@edu.univ-fcomte.fr>
2024-11-26 14:58:30 +0100

    referme les détails suite à un clic sur un `summary`

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,6 @@
 // *** ACCORDEON ***
+let accordeon = document.querySelector(".accordeon");
+
+accordeon.addEventListener("click", (evt) => {
+  accordeon.querySelectorAll("details").forEach((det) => (det.open = false));
+});
